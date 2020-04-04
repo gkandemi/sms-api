a8c3de2ea9f33f04f7efe21569d6710c0cdc72b2
gkandemi <gokhan@gkandemir.com>
2020-04-04 20:24:53 +0300

    new theme

--- public/client.js
+++ public/client.js
@@ -1,48 +1,54 @@
 let otpField = document.querySelector(".otpField");
 let verify = document.querySelector(".verify");
-let interval = null;
+const snackbarContent = document.getElementById("snackbar");
 
 verify.addEventListener("click", async e => {
   try {
-    // phoneForm.classList.remove("visible");
-    // otpForm.classList.add("visible");
-    // success.classList.remove("visible");
     await smsReceiver();
-    interval = setInterval(async () => {
-      console.log("Interval..");
-      await smsReceiver();
-    }, 2000);
   } catch (e) {
-    // snackbarAlert(e);
+    snackbar(e, 3000);
   }
 });
 
 const smsReceiver = async () => {
   if (!navigator.sms) return;
-  //   progress.determinate = false;
   try {
     const sms = await navigator.sms.receive();
-    alert(sms);
+    snackbar("Kontrol ediliyor...");
     const code = sms.content.match(/^[\s\S]*otp=([0-9]{6})[\s\S]*$/m)[1];
     if (code) {
-      clearInterval(interval);
-      alert(code);
       otpField.value = code;
       otpField.disabled = true;
-      //   cancel.disabled = true;
-      //   snackbarAlert("Verifying...");
-      //   setTimeout(_continue, 2500);
+      snackbar("Telefonunuz onaylanmıştır...", 3000);
     } else {
-      //   progress.determinate = true;
-      throw "Received the SMS, but failed to retrieve the OTP.";
+      snackbar("Received the SMS, but failed to retrieve the OTP.", 3000);
     }
   } catch (e) {
-    // progress.determinate = true;
     if (e.name !== "AbortError") {
-      throw "Failed to receive SMS";
+      snackbar("Failed to receive SMS", 3000);
     }
   }
 };
+
+function snackbar(content, expire) {
+  if (content === false) {
+    snackbarContent.classList.remove("show");
+    snackbarContent.classList.add("hide");
+  } else {
+    snackbarContent.classList.remove("hide");
+    snackbarContent.classList.add("show");
+
+    if (expire) {
+      setTimeout(() => {
+        snackbar(false);
+      }, 3000);
+    }
+
+    snackbarContent.innerHTML = content;
+  }
+}
+
+snackbar("Kontrol ediliyor...");
 
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("/sw.js");
