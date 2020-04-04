19a1d41babc694154c92cdbc8700dfdc0fdb6afb
gkandemi <gokhan@gkandemir.com>
2020-04-05 02:20:18 +0300

    theme updated

--- public/client.js
+++ public/client.js
@@ -1,9 +1,14 @@
-let otpField = document.querySelector(".otpField");
-let verify = document.querySelector(".verify");
+let otpInput = document.querySelector(".otpInput");
+const startBtn = document.querySelector(".startBtn");
 const snackbarContent = document.getElementById("snackbar");
+const homeScreen = document.querySelector(".homeScreen");
+const appContainer = document.querySelector(".appContainer");
 
-verify.addEventListener("click", async e => {
+startBtn.addEventListener("click", async e => {
   try {
+    homeScreen.style.display = "none";
+    appContainer.style.display = "flex";
+    snackbar("SMS Bekleniyor...");
     await smsReceiver();
   } catch (e) {
     snackbar(e, 3000);
@@ -14,12 +19,18 @@ const smsReceiver = async () => {
   if (!navigator.sms) return;
   try {
     const sms = await navigator.sms.receive();
-    snackbar("Kontrol ediliyor...");
     const code = sms.content.match(/^[\s\S]*otp=([0-9]{6})[\s\S]*$/m)[1];
     if (code) {
-      otpField.value = code;
-      otpField.disabled = true;
-      snackbar("Telefonunuz onaylanmıştır...", 3000);
+      otpInput.value = code;
+      otpInput.disabled = true;
+      snackbar("Kontrol ediliyor...");
+      setTimeout(() => {
+        if (!otpInput.checkValidity()) {
+          snackbar("Doğrulanamadı...");
+        } else {
+          snackbar("Telefonunuz onaylanmıştır...", 3000);
+        }
+      }, 1000);
     } else {
       snackbar("Received the SMS, but failed to retrieve the OTP.", 3000);
     }
@@ -46,10 +57,4 @@ function snackbar(content, expire) {
 
     snackbarContent.innerHTML = content;
   }
-}
-
-snackbar("Kontrol ediliyor...");
-
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/sw.js");
 }
