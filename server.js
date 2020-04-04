be3d3d6a0129f3072c88b15b12ffc328a33b9737
gkandemi <gokhan@gkandemir.com>
2020-04-04 19:14:48 +0300

    remove origintrial

--- server.js
+++ server.js
@@ -30,8 +30,7 @@ app.use((req, res, next) => {
 
 app.get("/", function(request, response) {
   response.render("index.html", {
-    AppHash: AppHash,
-    OriginTrial: process.env.OT_TOKEN
+    AppHash: AppHash
   });
 });
 
